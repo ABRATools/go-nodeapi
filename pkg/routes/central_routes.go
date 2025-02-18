6f8793815d4bd163222ef94facbd824feb2d7a36
soggycats <brandonramirez@unr.edu>
2025-02-17 20:46:03 -0800

    hopefully fix concurrency

--- pkg/routes/central_routes.go
+++ pkg/routes/central_routes.go
@@ -16,16 +16,17 @@ func RegisterCentralRoutes(router *gin.Engine) {
 	{
 		central.GET("node-info", func(c *gin.Context) {
 			retInfoChan := make(chan *hostdata.HostInfo)
+			errInfoChan := make(chan error)
 			getHostInfoCtx, getInfoCancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer getInfoCancel()
 			retContainersChan := make(chan []podmanapi.PodmanContainer)
+			errContainersChan := make(chan error)
 			getContainersCtx, getContainersCancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer getContainersCancel()
 			go func() {
 				hostInfo, err := hostdata.GetHostInfo()
 				if err != nil {
-					fmt.Printf("Error retrieving host info: %v\n", err)
-					c.String(http.StatusInternalServerError, "Error retrieving host info: %v", err)
+					errInfoChan <- fmt.Errorf("Error getting host info: %v", err)
 					return
 				}
 				retInfoChan <- hostInfo
@@ -33,31 +34,38 @@ func RegisterCentralRoutes(router *gin.Engine) {
 			go func() {
 				podmanContext, err := podmanapi.InitPodmanConnection()
 				if err != nil {
-					c.String(http.StatusInternalServerError, "Error connecting to Podman Socket: %v", err)
+					errContainersChan <- fmt.Errorf("Error connecting to Podman Socket: %v", err)
+					return
 				}
 				podmanContainers, err := podmanapi.ListPodmanContainers(podmanContext)
 				if err != nil {
-					c.String(http.StatusInternalServerError, "Error getting Podman Containers: %v", err)
+					errContainersChan <- fmt.Errorf("Error getting Podman Containers: %v", err)
 					return
 				}
 				retContainersChan <- podmanContainers
 			}()
+			var hostInfo *hostdata.HostInfo
+			var podmanContainers []podmanapi.PodmanContainer
 			select {
-			case <-retInfoChan:
+			case hostInfo = <-retInfoChan:
 				break
+			case err := <-errInfoChan:
+				c.String(http.StatusInternalServerError, "Error retrieving host info: %v", err)
+				return
 			case <-getHostInfoCtx.Done():
 				c.String(http.StatusInternalServerError, "Timeout retrieving host info")
 				return
 			}
 			select {
-			case <-retContainersChan:
+			case podmanContainers = <-retContainersChan:
 				break
+			case err := <-errContainersChan:
+				c.String(http.StatusInternalServerError, "Error retrieving containers: %v", err)
+				return
 			case <-getContainersCtx.Done():
 				c.String(http.StatusInternalServerError, "Timeout retrieving host info")
 				return
 			}
-			hostInfo := <-retInfoChan
-			podmanContainers := <-retContainersChan
 			hostInfo.NumContainers = len(podmanContainers)
 			c.JSON(http.StatusOK, gin.H{"host": hostInfo, "containers": podmanContainers})
 		})
